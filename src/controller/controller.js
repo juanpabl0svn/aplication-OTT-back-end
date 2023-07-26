0c3d1fb93ca557f73f69ebcff6399e4f04c96c26
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-07-26 11:02:11 -0500

    tenemos la creacion de usuarios y su autenticacion

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,4 @@
-const model = require("../database/schemas.js");
+const usersModel = require("../database/schemas.js");
 
 async function getUserData({ username, password }) {
   return await usersModel.find({ username, password });
@@ -8,18 +8,33 @@ async function userExists(username) {
   return await usersModel.findOne({ username });
 }
 
-async function createUser(req, res) {
+async function auth(req, res) {
   const { body } = req;
+
   try {
-    await usersModel.create(body);
-    return res.status(200).send("Created successfully");
+    const user = await usersModel.findOne(body);
+    res.json(user)
   } catch (err) {
-    return res.status(403).send("Error creating user");
+    res.json({ err})
   }
 }
+
+async function createUser(req, res) {
+  const { body } = req;
+
+  try {
+    await usersModel.create(body);
+    return res.status(200).json({ message: "Created successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(403).json({ message: "Error creating user" });
+  }
+}
+
 
 module.exports = {
   getUserData,
   userExists,
-  createUser
+  createUser,
+  auth
 };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
-const { getUserData, userExists, createUser } = require("../controller/controller.js");
+const { getUserData, userExists, createUser,auth } = require("../controller/controller.js");
 
 
 const router = Router()
+router.get('/',(req,res)=>{
+  res.send('hola')
+})
 
-
+router.post('/auth',auth)
 router.post('/create',createUser)
+
 
 
 module.exports = router
